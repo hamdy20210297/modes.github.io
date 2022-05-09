7597ce9c35e26d716098ec9d11c204247a30af58
Dalia321 <104854344+Dalia321@users.noreply.github.com>
2022-05-09 13:05:38 +0200

    dalia-first virsion

---
+++ Second Page(H)/script.js
@@ -0,0 +1,18 @@
+var MainImg= document.getElementById("MainImg"); 
+var smallimage=document.getElementByClassName("small-image);
+
+smallimage{0}.onclick =function() {
+MianImg.src=samlliamge{0}.src;
+}
+
+smallimage{1}.onclick=function() {
+MianImg.src=samllimage{1}.src;
+}
+
+smallimage{2}.onclick=function() {
+MianImg.src=samllimage{2}.src;
+}
+
+smallimage{3}.onclick=function() {
+MianImg.src=samllimage{3}.src;
+}
